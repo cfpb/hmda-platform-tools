b7831c85707420b54a20e01aee86458cc8e72366
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-02-15 13:11:05 -0800

    update readme and change links to static files to be relative for gh-pages

--- file-format-verification/src/js/containers/App.jsx
+++ file-format-verification/src/js/containers/App.jsx
@@ -15,7 +15,7 @@ export class AppContainer extends Component {
           <BannerUSA />
           <div className="usa-nav-container">
             <div className="usa-logo" id="logo">
-              <img src="/img/ffiec-logo.png" width="125px"/>
+              <img src="./img/ffiec-logo.png" width="125px"/>
             </div>
             <div className="WIP usa-alert-warning usa-text-small">
               <h5>This website is a work in progress</h5>
