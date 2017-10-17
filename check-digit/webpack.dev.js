5fc80b68f648c842dfae245d0058f5dd55793274
Andrew <awolfe76@gmail.com>
2017-10-17 16:06:10 -0400

    use the right NODE_ENV on builds

--- check-digit/webpack.dev.js
+++ check-digit/webpack.dev.js
@@ -1,11 +1,19 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 
 module.exports = merge(common, {
   entry: {
-    'main': './src/js/index.js'
+    main: './src/js/index.js'
   },
   output: {
-    filename: 'app.min.js',
-  }
+    filename: 'app.min.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    })
+  ]
 })
