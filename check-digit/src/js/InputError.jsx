570efe9e9a0afa4d5f5321da334608fd80d25fdf
Andrew <awolfe76@gmail.com>
2017-10-20 09:26:03 -0400

    allow form to be used for both getting and validating a check digit

--- check-digit/src/js/InputError.jsx
+++ check-digit/src/js/InputError.jsx
@@ -5,7 +5,7 @@ import Alert from './Alert.jsx'
 const InputError = ({ isSubmitted, errors }) => {
   if (isSubmitted && errors.length > 0) {
     return (
-      <Alert type="error" heading="ULI errors.">
+      <Alert type="error" heading="Sorry!">
         <ul>
           {errors.map((error, i) => {
             return <li key={i}>{error}</li>
