046066ad1c5d6f48eb4e5d8fc94ab6e02dfb6ac2
Andrew <awolfe76@gmail.com>
2017-12-06 14:25:13 -0500

    update intro text

--- check-digit/src/js/AppIntro.jsx
+++ check-digit/src/js/AppIntro.jsx
@@ -4,13 +4,9 @@ const AppIntro = () => {
   return [
     <h2 key={1}>Check Digit Tool</h2>,
     <p key={2} className="usa-font-lead">
-      The check digit tool performs two primary functions, generating a check
-      digit and validating a ULI. The tool will generate a two digit check digit
-      value based on the combination of LEI and Loan/Application ID provided.
-      The check digit value is then added to the LEI and Loan/Application ID to
-      form the loan or application's ULI. The tool will also validate a ULI by
-      verifying that the check digit value is calculated in accordance with
-      procedures defined in the regulation Appendix C to Part 1003.
+      You can use this tool for two functions. The first is generating a check
+      digit when you enter a loan ID. The second is validating any ULI that you
+      enter by making sure the check digit is calculated correctly.
     </p>
   ]
 }
