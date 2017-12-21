937f21e3c595c84dd2361df7168fff75a0d013e7
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-12-21 09:31:08 -0800

    fix comma

--- rate-spread/src/js/AppIntro.jsx
+++ rate-spread/src/js/AppIntro.jsx
@@ -15,7 +15,7 @@ const AppIntro = () => {
       The rate spread calculator generates the spread between the Annual
       Percentage Rate (APR) and a survey-based estimate of APRs currently
       offered on prime mortgage loans of a comparable type utilizing the
-      “Average Prime Offer Rates” fixed table or adjustable table , action
+      “Average Prime Offer Rates” fixed table or adjustable table, action
       taken, amortization type, lock-in date, APR, fixed term (loan maturity) or
       variable term (initial fixed-rate period), and reverse mortgage.
     </p>,
